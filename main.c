a3d7148648c6178b2e13bcec73c2771895f71e60
marcobrischetto@fecea.edu.ar <>
2021-05-25 17:11:57 -0300

    Calibracion del la celda de carga

--- main.c
+++ main.c
@@ -19,12 +19,13 @@ void main(void){
     hx711Init();
     while(1){
         acum=0;
-        for(uint8_t i=0;i<20;i++){
+        for(uint8_t i=0;i<10;i++){
             value=hx711Read();
             acum+=value;
             __delay_ms(100);
         }
-        acum/=20;
+        acum/=10;
+        //weight=acum;
         weight=M*acum+B;
         printf("%.2f\r\n",weight);  
         __delay_ms(500);
